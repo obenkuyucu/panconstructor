b2ea176ccee29f68f33bea5e0ce95d77143045bc
obenkuyucu <75614966+obenkuyucu@users.noreply.github.com>
2020-12-07 14:08:31 +0300

    Few corrections

--- panconstructor.py
+++ panconstructor.py
@@ -10,7 +10,7 @@ print("Last 4 digits: " + last4)
 
 print("Enter half MD5 hash:")
 half_hash = input()
-print(half_hash)
+print("Half of the MD5 hash: " + half_hash)
 
 initialPAN = first6 + '000000' + last4
 initialPAN = int(initialPAN)
@@ -19,5 +19,5 @@ for i in range(1000000):
 	hash_value_pan = hashlib.md5(str(initialPAN).encode()).hexdigest()
 	half_hash_pan = hash_value_pan[0:len(hash_value_pan)//2]
 	if half_hash_pan == half_hash:
-		print(initialPAN)
+		print("Constructed PAN: " + initialPAN)
 	initialPAN += 10000
