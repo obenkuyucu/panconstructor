3b230c8732be9de1c6b745f5f7c2f604144008c9
obenkuyucu <75614966+obenkuyucu@users.noreply.github.com>
2020-12-07 14:12:31 +0300

    Update panconstructor.py

--- panconstructor.py
+++ panconstructor.py
@@ -19,5 +19,6 @@ for i in range(1000000):
 	hash_value_pan = hashlib.md5(str(initialPAN).encode()).hexdigest()
 	half_hash_pan = hash_value_pan[0:len(hash_value_pan)//2]
 	if half_hash_pan == half_hash:
-		print("Constructed PAN: " + initialPAN)
+		print("Constructed PAN: ")
+		print(initialPAN)
 	initialPAN += 10000
