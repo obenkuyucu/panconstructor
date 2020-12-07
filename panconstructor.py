67d264b854c77fc1c8fd8fc5e486a1407bf5f970
obenkuyucu <75614966+obenkuyucu@users.noreply.github.com>
2020-12-07 14:40:47 +0300

    Update panconstructor.py
    Now supports any value of MD5. (must be first n digits of hash)
    Added input validation

--- panconstructor.py
+++ panconstructor.py
@@ -1,24 +1,38 @@
 import hashlib 
-
-print("Enter first 6 digits:")
-first6 = input()
+while True:
+    try:
+        first6 = int(input("Enter first 6 digits: "))
+    except ValueError:
+        print("Sorry, only integers")
+        continue
+    else:
+        break
+first6 = str(first6)
 print("First 6 digits: " + first6)
 
-print("Enter last 4 digits:")
-last4 = input()
+while True:
+    try:
+        last4 = int(input("Enter last 4 digits: "))
+    except ValueError:
+        print("Sorry, only integers")
+        continue
+    else:
+        break
+last4 = str(last4)
 print("Last 4 digits: " + last4)
 
-print("Enter half MD5 hash:")
-half_hash = input()
-print("Half of the MD5 hash: " + half_hash)
+hash_str = input("Enter any string of MD5 hash: ")
+print("Provided MD5 hash strings: " + hash_str)
 
 initialPAN = first6 + '000000' + last4
 initialPAN = int(initialPAN)
-
+tot_pan = 0
+print("Constructed PAN(s): ")
 for i in range(1000000): 
 	hash_value_pan = hashlib.md5(str(initialPAN).encode()).hexdigest()
-	half_hash_pan = hash_value_pan[0:len(hash_value_pan)//2]
-	if half_hash_pan == half_hash:
-		print("Constructed PAN: ")
+	split_hash_pan = hash_value_pan[0:len(hash_str)]
+	if split_hash_pan == hash_str:
 		print(initialPAN)
+		tot_pan = tot_pan + 1
 	initialPAN += 10000
+print("Total constructed PAN(s): " + format(tot_pan))
