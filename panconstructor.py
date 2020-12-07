e63ece49079e3253e08f9f3cf9e4242d0a0958d6
obenkuyucu <75614966+obenkuyucu@users.noreply.github.com>
2020-12-07 15:07:17 +0300

    Update panconstructor.py
    Added Luhn checks

--- panconstructor.py
+++ panconstructor.py
@@ -1,4 +1,33 @@
 import hashlib 
+
+def sum_digits(digit):
+    if digit < 10:
+        return digit
+    else:
+        sum = (digit % 10) + (digit // 10)
+        return sum
+
+def validate(cc_num):
+    # reverse the credit card number
+    cc_num = cc_num[::-1]
+    # convert to integer list
+    cc_num = [int(x) for x in cc_num]
+    # double every second digit
+    doubled_second_digit_list = list()
+    digits = list(enumerate(cc_num, start=1))
+    for index, digit in digits:
+        if index % 2 == 0:
+            doubled_second_digit_list.append(digit * 2)
+        else:
+            doubled_second_digit_list.append(digit)
+
+    # add the digits if any number is more than 9
+    doubled_second_digit_list = [sum_digits(x) for x in doubled_second_digit_list]
+    # sum all digits
+    sum_of_digits = sum(doubled_second_digit_list)
+    # return True or False
+    return sum_of_digits % 10 == 0
+
 while True:
     try:
         first6 = int(input("Enter first 6 digits: "))
@@ -32,7 +61,8 @@ for i in range(1000000):
 	hash_value_pan = hashlib.md5(str(initialPAN).encode()).hexdigest()
 	split_hash_pan = hash_value_pan[0:len(hash_str)]
 	if split_hash_pan == hash_str:
-		print(initialPAN)
-		tot_pan = tot_pan + 1
+		if validate(str(initialPAN)):
+			print(initialPAN)
+			tot_pan = tot_pan + 1
 	initialPAN += 10000
 print("Total constructed PAN(s): " + format(tot_pan))
